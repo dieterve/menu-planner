bccf50b00eb35c15412f28c7f2e418543be47aa2
dieterve <dieter@dieterve.be>
2012-02-12 13:30:01 +0100

    Move autoloader to a seperate file

---
+++ autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Autoloader
+ *
+ * Lets assume for now that our directory structure matches our namespace/class naming.
+ *
+ * For example:
+ * Application\Router is located at application/router.php
+ *
+ * @author Dieter Vanden Eynde <dieter@dieterve.be>
+ * @param string
+ */
+function autoLoader($className)
+{
+	$chunks = explode('\\', $className);
+	$chunks = array_map('strtolower', $chunks);
+
+	$path = __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $chunks) . '.php';
+
+	if(@file_exists($path))
+	{
+		require_once($path);
+	}
+}
+
+spl_autoload_register('autoLoader');
--- index.php
+++ index.php
@@ -1,26 +1,6 @@
 <?php
 
-/*
- * Autoloader
- *
- * Lets assume for now that our directory structure matches our namespace/class naming.
- *
- * For example:
- * Application\Router is located at application/router.php
- */
-function autoLoader($className)
-{
-	$chunks = explode('\\', $className);
-	$chunks = array_map('strtolower', $chunks);
-
-	$path = __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $chunks) . '.php';
-
-	if(@file_exists($path))
-	{
-		require_once($path);
-	}
-}
-spl_autoload_register('autoLoader');
+require_once('autoload.php');
 
 $router = new Application\Router();
 
