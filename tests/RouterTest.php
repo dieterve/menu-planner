70e54264ec16e083a24311e562552e79f0813419
dieterve <dieter@dieterve.be>
2012-02-12 14:14:16 +0100

    Add router mapping tests

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -27,4 +27,28 @@ class RouterTest extends PHPUnit_Framework_TestCase
 		$_SERVER['REQUEST_URI'] = '/path/deeper?param1=test&param2=more';
 		$this->assertEquals('/path/deeper', $router->getPath());
 	}
+
+	public function testMapping()
+	{
+		$router = new Application\Router();
+		$router->map('/path', function()
+		{
+			echo 'path';
+		});
+
+		$this->checkResponse($router, '/path', 'path');
+		$this->checkResponse($router, '/path?param1=test', 'path');
+		$this->checkResponse($router, '/unrelated?param1=test', '');
+	}
+
+	private function checkResponse($router, $uri, $expectedOutput)
+	{
+		$_SERVER['REQUEST_URI'] = $uri;
+
+		ob_start();
+		$router->process();
+		$output = ob_get_clean();
+
+		$this->assertEquals($expectedOutput, $output);
+	}
 }
