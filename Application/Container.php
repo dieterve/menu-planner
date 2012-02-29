4cb9d59b9226b352841e5ac5586ef74bb35752b0
dieterve <dieter@dieterve.be>
2012-02-29 21:54:31 +0100

    WIP: dependency injection container (fancy)

---
+++ Application/Container.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application;
+
+/**
+ * Dependency container.
+ *
+ * @author Dieter Vanden Eynde <dieter@dieterve.be>
+ */
+class Container
+{
+	/**
+	 * Shared services.
+	 *
+	 * @var array
+	 */
+	private static $shared = array();
+
+	public function getDatabaseService()
+	{
+
+	}
+
+	public function getTemplateService()
+	{
+
+	}
+}
