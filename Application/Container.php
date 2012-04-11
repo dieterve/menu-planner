577cc797d3ae264baf75647c0ca5b8978340dd59
dieterve <dieter@dieterve.be>
2012-04-11 22:02:45 +0200

    Modify DI container to work with magic getter/setter

--- Application/Container.php
+++ Application/Container.php
@@ -5,24 +5,39 @@ namespace Application;
 /**
  * Dependency container.
  *
+ * Based on a presentation of Fabien Potencier.
+ *
  * @author Dieter Vanden Eynde <dieter@dieterve.be>
  */
 class Container
 {
 	/**
-	 * Shared services.
-	 *
 	 * @var array
 	 */
-	private static $shared = array();
+	protected $values = array();
 
-	public function getDatabaseService()
+	public function __get($name)
 	{
+		if(!array_key_exists($name, $this->values))
+		{
+			throw new Exception('Value ' . $name . ' is not set.');
+		}
 
+		// call if its an instance or closure
+		if(is_callable($this->values[$name]))
+		{
+			return $this->values[$name]($this);
+		}
+
+		// static stored value
+		else
+		{
+			return $this->values[$name];
+		}
 	}
 
-	public function getTemplateService()
+	public function __set($name, $value)
 	{
-
+		$this->values[$name] = $value;
 	}
 }
