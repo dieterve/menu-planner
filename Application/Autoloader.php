fea72382a44d5a4f311c6aa080eb4b436b2c1cbd
dieterve <dieter@dieterve.be>
2012-04-21 19:55:35 +0200

    Autoloader: add adding of prefixes

--- Application/Autoloader.php
+++ Application/Autoloader.php
@@ -15,6 +15,11 @@ class Autoloader
 	private $namespaces = array();
 
 	/**
+	 * @var array
+	 */
+	private $prefixes = array();
+
+	/**
 	 * Require a class in the registered namespaces.
 	 *
 	 * @param string $class
@@ -22,19 +27,37 @@ class Autoloader
 	public function loadClass($class)
 	{
 		$class = (string) $class;
+		$file = '';
 
-		foreach($this->namespaces as $namespace => $path)
+		// namespace based class
+		if(strpos($class, '\\') !== false)
 		{
-			if(stripos($class, $namespace) !== false)
+			foreach($this->namespaces as $namespace => $path)
 			{
-				$file = $path . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-
-				if(file_exists($file))
+				if(stripos($class, $namespace) !== false)
 				{
-					require_once($file);
+					$file = $path . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
 					break;
 				}
 			}
+		}
+
+		// prefix based class
+		else
+		{
+			foreach($this->prefixes as $prefix => $path)
+			{
+				if(stripos($class, $prefix) !== false)
+				{
+					$file = $path . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
+					break;
+				}
+			}
+		}
+
+		if($file != '' && file_exists($file))
+		{
+			require_once($file);
 		}
 	}
 
@@ -47,6 +70,8 @@ class Autoloader
 	}
 
 	/**
+	 * Register the location of a namespace so it can be autoloaded.
+	 *
 	 * @param string $namespace
 	 * @param string $path
 	 */
@@ -70,6 +95,36 @@ class Autoloader
 		foreach($namespaces as $namespace => $path)
 		{
 			$this->registerNamespace($namespace, $path);
+		}
+	}
+
+	/**
+	 * Register the lcoation of classes starting with a given prefix.
+	 * PEAR namingconventions use this.
+	 *
+	 * @param string $prefix
+	 * @param string $path
+	 */
+	public function registerPrefix($prefix, $path)
+	{
+		$this->prefixes[(string) $prefix] = (string) $path;
+	}
+
+	/**
+	 * Register prefixes in bulk.
+	 * Format:
+	 * array(
+	 * 	'Prefix' => '/path/to/prefix',
+	 * 	'Prefix_Sub' => '/path/to/sub',
+	 * );
+	 *
+	 * @param array $prefixes
+	 */
+	public function registerPrefixes(array $prefixes)
+	{
+		foreach($prefixes as $prefix => $path)
+		{
+			$this->registerPrefix($prefix, $path);
 		}
 	}
 }
