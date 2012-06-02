aacf8042d62d1939c7417cbe4ebdf4be1195b3d6
dieterve <dieter@dieterve.be>
2012-06-02 17:14:32 +0200

    Autoloader: load the composer autoload maps

--- autoload.php
+++ autoload.php
@@ -8,6 +8,16 @@ $autoloader = new Autoloader();
 
 $autoloader->registerNamespace('Application', __DIR__);
 $autoloader->registerNamespace('Modules', __DIR__);
-$autoloader->registerPrefix('Twig', __DIR__ . '/vendor/twig/lib');
+
+/*
+ * Add vendor maps created by Composer.
+ *
+ * I do not use the autoloader because its much more cooler to use my own shit.
+ * Also, I just wanted to learn it myself.
+ * Also, I dont like stacking too	 many SPL autoloaders.
+ */
+$composerDir = __DIR__ . '/vendor/composer';
+$autoloader->registerNamespaces(require $composerDir . '/autoload_namespaces.php');
+$autoloader->registerNamespaces(require $composerDir . '/autoload_classmap.php');
 
 $autoloader->register();
